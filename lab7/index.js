5bc1d53e1a3d1f162578aeb2a162308dca398141
Svetlanlka <Svetlanlka@users.noreply.github.com>
2021-04-11 22:50:09 +0300

    add description

--- lab7/index.js
+++ lab7/index.js
@@ -1,19 +1,22 @@
-console.log("Script start");
-const empty_city = "City-17"
+console.log("start");
 const init_city = "Москва"
-const loading = "Загрузка..."
 
-// данные странички
-let city_field;         // поле для указания города
-let weather_field;      // поле с текущей погодой
-let weather_data;       // массив данных от сервера
-let weather_pict_field; // поле с картинкой
-let weather_text_field; // поле с текстом данных о погоде
+let weather_data;
+let weather_field = document.getElementById("temperatureLabel");
+let weather = document.getElementById("temperature");
+let city_field = document.getElementById("cityName");
 let wind = document.getElementById("wind");
 let humidity = document.getElementById("humidity");
 let pressure = document.getElementById("pressure");
+let todate = document.getElementById('current_time');
+let weather_min = document.getElementById("temperatureMin");
+let weather_max = document.getElementById("temperatureMax");
+let searchButton = document.getElementById('search_button');
+let searchInput = document.getElementById('search_city');
 
-// сеть
+let date = new Date();
+todate.innerHTML = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+
 let requestBase = 'https://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&units=metric&appid=20d168cec6dd572a4b246df42e4646e7';
 let request = new XMLHttpRequest();
 
@@ -21,29 +24,16 @@ function getUserText(data) {
     wind.innerHTML =  data['wind']['deg'] + 'm/c';
     humidity.innerHTML = data['main']['humidity'];
     pressure.innerHTML = data['main']['pressure'];
-    let text;
-    text = '<span">{temp}°С</span>\
-    <span>Feels {feels_like}°С</span>\
-    <div>min: {temp_min}°С / max: {temp_max}°С</div>\
-    </br>'
-    .replace("{temp}", data['main']['temp'])
-    .replace("{feels_like}", data['main']['feels_like'])
-    .replace("{temp_min}", data['main']['temp_min'])
-    .replace("{temp_max}", data['main']['temp_max'])
-    return text;
+    weather_min.innerHTML = 'min:' + data['main']['temp_min'] + '°С';
+    weather_max.innerHTML =  'max:' + data['main']['temp_max'] + '°С';
+    weather.innerHTML =   data['main']['temp'] + '°С';
+    weather_field.innerHTML = weather_data['weather'][0]['description'];
 }
 
 function getCityWeather(city) {
-    city_field = document.getElementById("cityName");
-    console.log("current city: "+ city_field.innerText + " | trying to set " + city);
-    
-    let prev_req = weather_data;
-
-    weather_field = document.getElementById("temperature");
+    city_field.innerText = city;
+    let req = weather_data;
     weather_field.innerHTML = loading;
-    //weather_pict_field = document.getElementById("weather_pict");
-    //weather_pict_field.innerHTML = '<img src="img/loading.png"></img>';
-    weather_text_field = document.getElementById("temperatureLabel");
     
     let prom = new Promise((resolve, reject) => {
        requestURL = requestBase.replace('{city}', city);
@@ -62,182 +52,28 @@ function getCityWeather(city) {
 
     prom.then(
         (x) => {      
-            console.log("promise successed");
-
             weather_data = x;
-            console.log("got weather data | weather_main: " + weather_data['weather'][0]['main']);
-            console.log("got weather data | weather_description: " + weather_data['weather'][0]['description']);
-            console.log("got weather data | weather_icon: " + weather_data['weather'][0]['icon']);
-            console.log("got weather data | name: " + weather_data['name']);
-
-            weather_field.innerHTML = weather_data['weather'][0]['description'];
-            // weather_pict_field.innerHTML = '<img src="img/{icon}.png">'.replace("{icon}",  weather_data['weather'][0]["icon"]);
-            weather_text_field.innerHTML = getUserText(weather_data);
-            city_field.innerHTML = weather_data['name'];
+            getUserText(weather_data);
         },
         (x) => {
             console.log("promise failed");
             alert(x.message);
-            weather_data = prev_req;
-            // восстанавливаем данные, на месте которых сейчас "загрузка"
-            weather_field.innerHTML = weather_data['weather'][0]['description']; 
-            // weather_pict_field.innerHTML = '<img src="img/{icon}.png">'.replace("{icon}",  weather_data['weather'][0]["icon"]);
+            weather_data = req;
         }
     );
 }
 
-// кнопка поиска города
-const searchButton = document.getElementById('search_button');
-const searchInput = document.getElementById('search_city');
 searchButton.addEventListener('click', () => {
-    const inputValue = searchInput.value;
+    let inputValue = searchInput.value;
+    city_field.innerText = inputValue;
+    init_city = inputValue;
+
     console.log("search: " + inputValue);
     if (inputValue === "")
-        return;
+         return;
     getCityWeather(inputValue);
+    console.log("end click");
 });
 
 getCityWeather(init_city);
-console.log("Script end");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let input_city = document.getElementById('search_city');
-// let city = document.getElementById('city_name');
-// let search_button = document.getElementById('search_button');
-
-// console.log(input_city);
-// city.innerHTML = input_city.value;
-
-// // let wh_val = document.getElementById('wh_val');
-// // let wh_ico = document.getElementById('weather_ico');
-// // let mon_val = document.getElementById('mon_val');
-// // let mid_val = document.getElementById('mid_val');
-// // let weather;
-// // let base = document.getElementById('base');
-// // let er = document.getElementById('er');
-
-// let date = new Date();
-// let d = date.getDate();
-// let m = date.getMonth();
-// let y = date.getFullYear();
-
-// let todate = document.getElementById('current_time');
-// todate.innerHTML = d + '.' + m + '.' + y;
-
-// console.log("search begin");
-// search_button.addEventListener('click', () => {
-//   console.log("click begin");
-//     let promise = fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input_city.value + '&units=metric&lang=ru&appid=20d168cec6dd572a4b246df42e4646e7')
-//         .then(response => {
-//             if (!response.ok) {
-//                 console.log("error");
-//                 return response.json()
-//                 // er.style.display = 'block';
-//                 // base.style.display = 'none';
-//             } else {
-//                 res.json().then(data => {
-//                 //         // base.style.display = 'flex';
-//                 //         // er.style.display = 'none';
-//                         city.innerHTML = data.name;
-//                 //         // console.log(data);
-//                 //         // if (data.main.temp > 0) data.main.temp = '+' + data.main.temp;
-//                 //         // if (data.main.temp_max > 0) data.main.temp_max = '+' + data.main.temp_max;
-//                 //         // if (data.main.temp_min > 0) data.main.temp_min = '+' + data.main.temp_min;
-//                 //         // wh_val.innerHTML = data.main.temp;
-//                 //         // wh_ico.src = './img/animated/' + data.weather[0].icon + '.svg';
-//                 //         // mon_val.innerHTML = data.main.temp_max;
-//                 //         // mid_val.innerHTML = data.main.temp_min;
-//                 //         // console.log(wh_ico.src);
-//                 })
-//                 .catch(err => console.error(err));
-//             }
-
-//         })
-//         // .catch(console.error);
-//         promise.then(data=>console.log(data)).catch(err=>console.log(err));
-//         console.log(promise);
-// });
-
-
-// console.log("script end");
-
-
-
-/*async function getData(url) {
-    return fetch(url)
-      .then((data) => data.json())
-      .then((data) => data);
-}
-
-const init_city = "Moscow";
-
-let city = init_city;
-let city_field;
-let weather_data;
-
-let requestBase = 'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=20d168cec6dd572a4b246df42e4646e7';
-let request = new XMLHttpRequest();
-
-function getWeatherForeСast() {
-    requestURL = requestBase.replace('{city}', city);
-
-    request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-
-    request.onload = function() {
-        weather_data = request.response;
-        console.log("got weather data | weather in " + city + "  is: " + weather_data['weather'][0]['main']);
-    }
-
-    getCity(weather_data);
-}
-
-function getCity(weather_container) {
-    city_field = document.getElementById("city_name");
-    //let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-    // let request = new XMLHttpRequest();
-    // request.open('GET', requestURL);
-    // request.responseType = 'json';
-    // request.send();
-
-    // request.onload = function() {
-    //     weather_data = request.response;
-    //     console.log(weather_data);
-    // }
-
-    city = weather_container.name;
-    city_field.innerHTML = city;
-}
-
-document.getElementById("search_button").addEventListener("click", () => {
-    city = document.getElementById("search_city").value;
-  
-    getData(weather_url)
-      .then((data) => {
-        FillWeather(data);
-      })
-      .catch((err) => {
-        ShowErr(err);
-        return;
-      });
-    getData(forecast_url).then((data) => {
-      FillForecast(data);
-    });
-});
-
-setTimeout(getCity(), 3000);
-getWeatherForeСast();*/
+console.log("end");
